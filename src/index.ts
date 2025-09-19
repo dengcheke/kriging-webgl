ea1f00b12488d5110a30c721c3550084ab9fd381
dengcheke <871758573@qq.com>
2025-09-19 16:11:08 +0800

    expose glsl

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 
 export { predict, train, type KrigingModel, type Variogram } from './kriging';
 export { generate_WEBGL  } from './webgl/generate';
-export { glsl_pack } from './webgl/glsl';
+export { glsl_pack, glsl_colorMapping } from './webgl/glsl';
 export { createColorMappingObject, createVariogramObject } from './webgl/utils';
 
